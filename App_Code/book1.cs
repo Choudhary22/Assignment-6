83143869c9451d3b2352fc686f222468c124df18
Choudhary22 <46981352+Choudhary22@users.noreply.github.com>
2019-08-01 12:46:12 +1200

    Add files via upload

---
+++ App_Code/book1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+/// <summary>
+/// Summary description for book
+/// </summary>
+public class book1
+{
+    SqlDataAdapter adap = new SqlDataAdapter();
+    SqlConnection con = new SqlConnection();
+    SqlCommand com = new SqlCommand();
+    DataSet d = new DataSet();
+	
+    public book1()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    public string connectionstring()
+    {
+        string str= @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hp\Desktop\WebSite36\App_Data\hotel.mdf;Integrated Security=True;Connect Timeout=30";
+        return (str);
+    }
+     
+     
+
+}
